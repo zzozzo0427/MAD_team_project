820b33a60df3654c5c84d6ad4e43fdb80618b2b9
zzozzo0427 <22000104@handong.ac.kr>
2024-05-27 22:01:07 +0900

    map에 navbar 추가

--- lib/map.dart
+++ lib/map.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mad_real_project/main.dart';
 import 'package:map_launcher/map_launcher.dart';
+import 'package:provider/provider.dart';
+
+import 'home.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -9,7 +13,6 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   void _showMapsDialog(BuildContext context, Coords coords) async {
     final availableMaps = await MapLauncher.installedMaps;
-
     showModalBottomSheet(
       context: context,
       builder: (context) {
@@ -43,6 +46,7 @@ class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     final coords = Coords(37.7749, -122.4194); // 샌프란시스코 위치
+    final navBarModel = Provider.of<NavBarModel>(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -57,6 +61,52 @@ class _MapScreenState extends State<MapScreen> {
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: Consumer<NavBarModel>(
+        builder: (context, navBarModel, child) {
+          return BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.videogame_asset),
+                label: 'Game',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.group),
+                label: 'Community',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.map),
+                label: 'Map',
+              ),
+            ],
+            currentIndex: navBarModel.currentIndex,
+            selectedItemColor: Colors.green,
+            onTap: (index) {
+              navBarModel.setCurrentIndex(index);
+              // Handle navigation on tap
+              if (index == 0) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                );
+              // } else if (index == 1) {
+              //   Navigator.push(
+              //     context,
+              //     MaterialPageRoute(builder: (context) => GameScreen()),
+              //   );
+              // } else if (index == 2) {
+              //   Navigator.push(
+              //     context,
+              //     MaterialPageRoute(builder: (context) => CommunityScreen()),
+              //   );
+              }
+            },
+          );
+        },
       ),
     );
   }
