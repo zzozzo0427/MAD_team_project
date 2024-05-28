de9222c04c07d1bcaca5bbc516288fb7738f29e2
22100275 <103886797+22100275@users.noreply.github.com>
2024-05-28 14:38:46 +0900

    홈다트 백엔드연결
    이름과 point로 레벨 표시

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mad_real_project/main.dart';
-import 'package:mad_real_project/rank.dart';
 import 'package:provider/provider.dart';
 import 'package:lottie/lottie.dart';
+import 'rank.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String userName = '';
+  int userPoints = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
+  Future<void> _fetchUserData() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+      setState(() {
+        userName = userDoc['Name'];
+        userPoints = userDoc['MyPoints'];
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,8 +58,7 @@ class HomeScreen extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
             child: IconButton(
-              icon:
-                  Icon(Icons.settings, color: Colors.black), // 세팅 아이콘을 오른쪽에 배치
+              icon: Icon(Icons.settings, color: Colors.black), // 세팅 아이콘을 오른쪽에 배치
               onPressed: () {
                 // Settings button action
               },
@@ -51,7 +77,7 @@ class HomeScreen extends StatelessWidget {
                   children: [
                     SizedBox(height: 20),
                     Text(
-                      'Angelic Lung',
+                      userName.isNotEmpty ? userName : 'Loading...',
                       style: TextStyle(
                         fontSize: 24,
                         fontWeight: FontWeight.bold,
@@ -70,7 +96,7 @@ class HomeScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 20),
                 Text(
-                  'LV. 81',
+                  'LV. ${userPoints ~/ 50}',
                   style: TextStyle(
                     fontSize: 20,
                     color: Colors.green,
@@ -78,7 +104,7 @@ class HomeScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 20),
                 LinearProgressIndicator(
-                  value: 0.81, // LV. 81을 나타내는 값 (0.0 ~ 1.0)
+                  value: (userPoints % 50) / 50, // MyPoints에서 50 나눈 값의 비율
                   backgroundColor: Colors.grey[300],
                   color: Colors.green,
                 ),
@@ -90,11 +116,10 @@ class HomeScreen extends StatelessWidget {
             right: 20,
             child: ElevatedButton(
               onPressed: () {
-                // Rank button action
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LeaderBoard()),
-                  );
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LeaderBoard()),
+                );
               },
               child: Text(
                 'Rank',
