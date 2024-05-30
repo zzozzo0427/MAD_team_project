ba1eb2040357637552418ce82a7e1954262100ba
22100275 <103886797+22100275@users.noreply.github.com>
2024-05-30 23:19:47 +0900

    메인 수정

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,69 @@ import 'app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:provider/provider.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    // Firebase 초기화 후 사용자 로그인 상태 확인
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user = auth.currentUser;
+
+    // 사용자가 로그인되어 있는 경우만 챌린지 체크
+    if (user != null) {
+      await checkAndUpdateChallenges(user);
+    }
+  } catch (e) {
+    print("Firebase 초기화 오류: $e");
+  }
+
   runApp(
     ChangeNotifierProvider(
       create: (_) => NavBarModel(),
       child: MyApp(),
     ),
   );
+}
+
+Future<void> checkAndUpdateChallenges(User user) async {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  CollectionReference challengesRef = firestore.collection('challenge');
+  CollectionReference usersRef = firestore.collection('users');
+
+  DateTime now = DateTime.now();
+  try {
+    QuerySnapshot querySnapshot = await challengesRef
+        .where('overDate', isLessThanOrEqualTo: now)
+        .where('uid', isEqualTo: user.uid)
+        .get();
+
+    for (var doc in querySnapshot.docs) {
+      var data = doc.data() as Map<String, dynamic>;
+      String uid = data['uid'];
+      int overPoint = data['overPoint'];
+
+      DocumentReference userDoc = usersRef.doc(uid);
+      await firestore.runTransaction((transaction) async {
+        DocumentSnapshot snapshot = await transaction.get(userDoc);
+
+        if (!snapshot.exists) {
+          throw Exception("User does not exist!");
+        }
+
+        int newPoints = (snapshot.data() as Map<String, dynamic>)['MyPoints'] + overPoint;
+        transaction.update(userDoc, {'MyPoints': newPoints});
+        transaction.delete(doc.reference);
+      });
+    }
+  } catch (e) {
+    print("챌린지 업데이트 오류: $e");
+  }
 }
 
 class NavBarModel extends ChangeNotifier {
