9131f5da51e9be134aaa33966bfde9c6e3c1416f
22100275 <103886797+22100275@users.noreply.github.com>
2024-06-02 15:27:32 +0900

    STT 기능 추가
    근데 실행되는지는 모르겠음
    휴대폰으로 해봐야 할 듯

--- lib/add.dart
+++ lib/add.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class AddPostPage extends StatefulWidget {
   @override
@@ -9,6 +13,75 @@ class AddPostPage extends StatefulWidget {
 
 class _AddPostPageState extends State<AddPostPage> {
   final TextEditingController _textController = TextEditingController();
+  final SpeechToText _speechToText = SpeechToText();
+  bool _hasSpeech = false;
+  String _lastWords = '';
+  String _currentLocaleId = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _initSpeechState();
+  }
+
+  Future<void> _initSpeechState() async {
+    try {
+      var hasSpeech = await _speechToText.initialize(
+        onError: errorListener,
+        onStatus: statusListener,
+      );
+      if (hasSpeech) {
+        var systemLocale = await _speechToText.systemLocale();
+        _currentLocaleId = systemLocale?.localeId ?? '';
+      }
+
+      if (!mounted) return;
+
+      setState(() {
+        _hasSpeech = hasSpeech;
+      });
+    } catch (e) {
+      print("Speech initialization failed: $e");
+    }
+  }
+
+  void startListening() {
+    _lastWords = '';
+    _speechToText.listen(
+      onResult: resultListener,
+      listenFor: Duration(seconds: 30),
+      localeId: _currentLocaleId,
+      onSoundLevelChange: soundLevelListener,
+      cancelOnError: true,
+    );
+    setState(() {});
+  }
+
+  void resultListener(SpeechRecognitionResult result) {
+    setState(() {
+      _lastWords = result.recognizedWords;
+      _textController.text = _lastWords;
+    });
+  }
+
+  void soundLevelListener(double level) {
+    // Handle sound level changes if necessary
+  }
+
+  void errorListener(SpeechRecognitionError error) {
+    setState(() {
+      // Handle error if necessary
+    });
+  }
+
+  void statusListener(String status) {
+    print('Status listener: $status');
+    if (_currentLocaleId != status) {
+      setState(() {
+        _currentLocaleId = status;
+      });
+    }
+  }
 
   void _submitPost() async {
     final currentUser = FirebaseAuth.instance.currentUser;
@@ -28,7 +101,7 @@ class _AddPostPageState extends State<AddPostPage> {
   @override
   Widget build(BuildContext context) {
     final currentUser = FirebaseAuth.instance.currentUser;
-    final String profileImageUrl = currentUser?.photoURL ?? 'https://www.example.com/default_profile_image.png'; // 기본 이미지 URL로 대체
+    final String profileImageUrl = currentUser?.photoURL ?? 'https://www.example.com/default_profile_image.png';
 
     return Scaffold(
       appBar: AppBar(
@@ -75,7 +148,193 @@ class _AddPostPageState extends State<AddPostPage> {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: !_hasSpeech || _speechToText.isListening
+            ? null
+            : startListening,
+        child: Icon(Icons.mic),
+      ),
       backgroundColor: Colors.green[50],
     );
+  }
+}
+
+class Stt extends StatelessWidget {
+  const Stt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: SttPage(),
+    );
+  }
+}
+
+class SttPage extends StatefulWidget {
+  const SttPage({Key? key}) : super(key: key);
+
+  @override
+  _SttPageState createState() => _SttPageState();
+}
+
+class _SttPageState extends State<SttPage> {
+  bool _hasSpeech = false;
+  bool _logEvents = false;
+  double level = 0.0;
+  double minSoundLevel = 50000;
+  double maxSoundLevel = -50000;
+  String lastWords = '';
+  String lastError = '';
+  String lastStatus = '';
+  String _currentLocaleId = '';
+  List<LocaleName> _localeNames = [];
+  final SpeechToText speech = SpeechToText();
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechState();
+  }
+
+  Future<void> initSpeechState() async {
+    _logEvent('Initialize');
+    var hasSpeech = await speech.initialize(
+        onError: errorListener,
+        onStatus: statusListener,
+        debugLogging: true,
+        finalTimeout: Duration(milliseconds: 0));
+    if (hasSpeech) {
+      _localeNames = await speech.locales();
+      var systemLocale = await speech.systemLocale();
+      _currentLocaleId = systemLocale?.localeId ?? '';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _hasSpeech = hasSpeech;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(children: [
+          Expanded(
+            flex: 4,
+            child: Container(
+              padding: EdgeInsets.only(top: 100.0),
+              child: Column(
+                children: <Widget>[
+                  const Center(
+                    child: Text(
+                      '음성 인식 결과',
+                      style: TextStyle(fontSize: 22.0),
+                    ),
+                  ),
+                  Expanded(
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          color: Colors.grey[200],
+                          child: Center(
+                            child: Text(
+                              lastWords,
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(bottom: 50.0),
+            child: Column(
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: !_hasSpeech || speech.isListening
+                          ? null
+                          : startListening,
+                      icon: Icon(Icons.mic),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.only(top: 0.0),
+                        child: const Text("click!",
+                            style: TextStyle(
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.blue)))
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  void startListening() {
+    _logEvent('start listening');
+    lastWords = '';
+    lastError = '';
+    speech.listen(
+        onResult: resultListener,
+        listenFor: Duration(seconds: 30),
+        pauseFor: Duration(seconds: 5),
+        partialResults: true,
+        localeId: _currentLocaleId,
+        onSoundLevelChange: soundLevelListener,
+        cancelOnError: true,
+        listenMode: ListenMode.confirmation);
+    setState(() {});
+  }
+
+  void resultListener(SpeechRecognitionResult result) {
+    _logEvent(
+        'Result listener final: ${result.finalResult}, words: ${result.recognizedWords}');
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  void soundLevelListener(double level) {
+    minSoundLevel = min(minSoundLevel, level);
+    maxSoundLevel = max(maxSoundLevel, level);
+    setState(() {
+      this.level = level;
+    });
+  }
+
+  void errorListener(SpeechRecognitionError error) {
+    _logEvent(
+        'Received error status: $error, listening: ${speech.isListening}');
+    setState(() {
+      lastError = '${error.errorMsg} - ${error.permanent}';
+    });
+  }
+
+  void statusListener(String status) {
+    _logEvent(
+        'Received listener status: $status, listening: ${speech.isListening}');
+    setState(() {
+      lastStatus = '$status';
+    });
+  }
+
+  void _logEvent(String eventDescription) {
+    if (_logEvents) {
+      var eventTime = DateTime.now().toIso8601String();
+      print('$eventTime $eventDescription');
+    }
   }
 }
