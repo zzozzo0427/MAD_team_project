2e055766f769f9fa38f50266620659264316f26d
zzozzo0427 <22000104@handong.ac.kr>
2024-06-04 17:31:03 +0900

    edit map, add weather

--- lib/map.dart
+++ lib/map.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_naver_map/flutter_naver_map.dart'; 
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -10,11 +10,48 @@ class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          title: Text('포항시 보건소 위치'),
+        ),
         body: NaverMap(
-          options: const NaverMapViewOptions(),
+          options: NaverMapViewOptions(
+            initialCameraPosition: const NCameraPosition(
+              target: NLatLng(36.0425, 129.3544),
+              zoom: 12,
+            ),
+          ),
           onMapReady: (controller) {
-            print("네이버 맵 로딩됨!");
+            final markerNamgu = NMarker(
+              id: 'namgu',
+              position: const NLatLng(36.0192, 129.3431),
+            );
+            final markerBukgu = NMarker(
+              id: 'bukgu',
+              position: const NLatLng(36.0658, 129.3657),
+            );
+
+            controller.addOverlayAll({markerNamgu, markerBukgu});
+
+            final onMarkerInfoWindowNamgu = NInfoWindow.onMarker(
+              id: markerNamgu.info.id,
+              text: "포항시 남구 보건소",
+            );
+            final onMarkerInfoWindowBukgu = NInfoWindow.onMarker(
+              id: markerBukgu.info.id,
+              text: "포항시 북구 보건소",
+            );
+
+            markerNamgu.openInfoWindow(onMarkerInfoWindowNamgu);
+            markerBukgu.openInfoWindow(onMarkerInfoWindowBukgu);
           },
         ),
       ),
--- lib/weather.dart
+++ lib/weather.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:lottie/lottie.dart';
 
 class WeatherScreen extends StatefulWidget {
   @override
@@ -37,27 +38,69 @@ class _WeatherScreenState extends State<WeatherScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Weather in $cityName'),
+        backgroundColor: Colors.green,
       ),
-      body: weatherData == null
-          ? Center(child: CircularProgressIndicator())
-          : Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '${weatherData!['main']['temp']}°C',
-                  style: TextStyle(fontSize: 40),
+      body: Center(
+        child: weatherData == null
+            ? CircularProgressIndicator()
+            : Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${weatherData!['main']['temp']}°C',
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      weatherData!['weather'][0]['description'],
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                    SizedBox(height: 20),
+                    if (weatherData!['weather'][0]['main'] != 'Rain' &&
+                        weatherData!['main']['temp'] >= 10)
+                      Column(
+                        children: [
+                          Lottie.asset('assets/running_man.json', height: 200),
+                          SizedBox(height: 20),
+                          Text(
+                            '오늘은 날씨가 좋아요! 담배는 잊고 밖에서 뛰어 놀아봅시다!',
+                            style: TextStyle(fontSize: 18, color: Colors.green),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      )
+                    else
+                      Column(
+                        children: [
+                          Lottie.asset('assets/reading.json', height: 200),
+                          SizedBox(height: 20),
+                          Text(
+                            '오늘은 집에서 독서를 하며 마음의 평화를 얻어보아요',
+                            style: TextStyle(fontSize: 18, color: Colors.blue),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: fetchWeather,
+                      child: Text('Refresh'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        foregroundColor: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  weatherData!['weather'][0]['description'],
-                  style: TextStyle(fontSize: 20),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: fetchWeather,
-                  child: Text('Refresh'),
-                ),
-              ],
-            ),
+              ),
+      ),
     );
   }
 }
+
